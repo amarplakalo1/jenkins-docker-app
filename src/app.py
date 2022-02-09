934045ed444a1a4df2d8b0e9f91984bfdef2cd19
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2022-02-09 12:20:56 +0000

    I made a template of the app program so I have a starting point

--- src/app.py
+++ src/app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+
+app = Flask(__name__) # helps determine the root path
+
+@app.route('/') # homepage of website
+def index():
+    return 'This is the homepage'
+
+@app.route('/about')
+def about():
+    return '<h2>This is the about page</h2>'
+
+
+if __name__ == "__main__": # Start the server
+    app.run(debug=True)
 
