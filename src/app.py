ead7a7da4ffb535cafdd02ff38b5c175c3b360f5
aplakalo <aplakalo@shutterstock.com>
2022-02-09 14:52:00 +0000

    Added two new routes - adding item and removing item from list. Very basic start to my app project

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__) # helps determine the root path
@@ -11,7 +11,23 @@ def index():
 def about():
     return '<h2>This is the about page of the website</h2>'
 
+@app.route('/addingitem')
+def adding_item():
+    for i in range(0,3):
+        list1.append(input("Enter an item in your to-do list: "))
+    return str(list1)
+
+@app.route('/removingitem')
+def remove_item():
+    item_to_remove_from_list = input("What item do you wish to remove from the list?")
+    list1.remove(item_to_remove_from_list)
+    return str(list1)
+
+@app.route("/health", methods=['GET'])
+def health_ep():
+    return 'Health : Excellent'
+    
 
 if __name__ == "__main__": # Start the server
+    list1 = []
     app.run(debug=True)
-
